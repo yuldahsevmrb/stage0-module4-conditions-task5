346ef4b828fb6b4e03dcf213853da6afd3e205c9
Whocares <yuldashevmrb@gmail.com>
2022-09-06 17:08:06 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        if( year%400 == 0){
+            System.out.println("leap");
+        } else if (year%100 == 0 ){
+            System.out.println("not leap");
+        } else if (year % 4 == 0){
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
